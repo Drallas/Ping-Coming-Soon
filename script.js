b7af047d386755268dbd16713a09ac882966f114
drallas <drallas@icloud.com>
2021-03-14 22:25:22 +0100

    adding form validation and fixing social icon :hover state.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // Simple Check to validate form.
-function checkEmail() {
-  var email = document.getElementById("email");
-  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
+const email = document.getElementById('email');
+const form = document.querySelector('form');
+
+
+// Function
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function checkEmail() {
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   if (email.value === "") {
-    alert("Whoops! It looks like you forgot to add your email!");
+    showError(email, 'Whoops! It looks like you forgot to add your email!');
   } else if (!filter.test(email.value)) {
-    alert("Please provide a valid email address!");
+    showError(email, 'Please provide a valid email address!');
     email.focus;
     return false;
   }
 }
+
+// Evenlistener 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    emailInput = email.value;
+    checkEmail();
+})
